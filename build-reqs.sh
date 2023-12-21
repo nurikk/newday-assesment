334b4f35a0d4d85accb6866a2699bcbfa67c4ff3
John Doe <nurikk@users.noreply.github.com>
2023-12-21 01:48:55 +0000

    Remove unnecessary code and update build requirements

--- build-reqs.sh
+++ build-reqs.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-#mkdir -p /opt/data
-#wget http://files.grouplens.org/datasets/movielens/ml-1m.zip -O /tmp/ml-1m.zip
-#unzip /tmp/ml-1m.zip -d /data
-
 pex --python=python3 --inherit-path=prefer "$(pipenv requirements)" -o ./dist/jobs.pex -D .
