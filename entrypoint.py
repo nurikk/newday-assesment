d3d5da56cf1e66292a9e490eafa513bc04601a5a
John Doe <nurikk@users.noreply.github.com>
2023-12-21 01:09:05 +0000

    Add logging and update print statements

--- entrypoint.py
+++ entrypoint.py
@@ -1,4 +1,6 @@
+import pyspark
 import argparse
+import logging
 import os
 import importlib
 import time
@@ -7,17 +9,21 @@ import sys
 import findspark
 
 findspark.init()
-import pyspark
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Run a PySpark job')
-    parser.add_argument('--job', type=str, required=True, dest='job_name', help="The name of the job module you want to run.")
+    parser.add_argument('--job', type=str, required=True, dest='job_name',
+                        help="The name of the job module you want to run.")
     parser.add_argument('--job-arg', type=str, action='append',
                         help="Extra arguments to send to the PySpark job, the format is space delimited string. example: --job-arg foo=bar --job-arg date=2012-12-12\ 12:12:12")
     parser.add_argument('--env-var', type=str, action='append',
                         help="ENV vars to be passed to cluster. example: --env-var foo=bar --env-var ENV=production")
     return parser.parse_args()
+
 
 def create_spark_session(job_name: str, env_vars: dict) -> pyspark.sql.SparkSession:
     temp_spark = pyspark.sql.SparkSession.builder.appName(job_name)
@@ -28,6 +34,7 @@ def create_spark_session(job_name: str, env_vars: dict) -> pyspark.sql.SparkSess
 
     return temp_spark.getOrCreate()
 
+
 def collect_args(args: list) -> dict:
     dict_args = {}
     args = args or []
@@ -36,10 +43,12 @@ def collect_args(args: list) -> dict:
         dict_args[key] = value
     return dict_args
 
+
 def main(args: argparse.Namespace) -> None:
     job_args = collect_args(args.job_arg)
     env_vars = collect_args(args.env_var)
-    print('\nRunning job %s...\njob-args is %s\nenv-vars is %s\n' % (args.job_name, job_args, env_vars))
+    logger.info('\nRunning job %s...\njob-args is %s\nenv-vars is %s\n' %
+                (args.job_name, job_args, env_vars))
 
     spark = create_spark_session(args.job_name, env_vars)
     job_module = importlib.import_module('jobs.%s' % args.job_name)
@@ -47,11 +56,13 @@ def main(args: argparse.Namespace) -> None:
     try:
         job_module.perform(spark, job_args)
     except Exception as e:
-        print(f'Error: {str(e)}\nTrace: {traceback.format_exc()}')
+        logger.error(f'Error: {str(e)}\nTrace: {traceback.format_exc()}')
         sys.exit(1)
 
     end = time.time()
-    print("\nExecution of job %s took %s seconds" % (args.job_name, end - start))
+    logger.info("\nExecution of job %s took %s seconds" %
+                (args.job_name, end - start))
+
 
 if __name__ == "__main__":
     args = parse_arguments()
