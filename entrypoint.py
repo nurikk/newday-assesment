e6a03eca0aaba982cc901595120efd52c75f0370
John Doe <nurikk@users.noreply.github.com>
2023-12-21 01:06:24 +0000

    Refactor entrypoint.py and add unit tests

--- entrypoint.py
+++ entrypoint.py
@@ -1,22 +1,26 @@
 import argparse
+import os
 import importlib
 import time
-import os
-import sys
 import traceback
-
+import sys
 import findspark
 
 findspark.init()
-
 import pyspark
 
 
-def create_spark_session(job_name, env_vars):
-    temp_spark = pyspark.sql.SparkSession \
-        .builder \
-        .appName(job_name)
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Run a PySpark job')
+    parser.add_argument('--job', type=str, required=True, dest='job_name', help="The name of the job module you want to run.")
+    parser.add_argument('--job-arg', type=str, action='append',
+                        help="Extra arguments to send to the PySpark job, the format is space delimited string. example: --job-arg foo=bar --job-arg date=2012-12-12\ 12:12:12")
+    parser.add_argument('--env-var', type=str, action='append',
+                        help="ENV vars to be passed to cluster. example: --env-var foo=bar --env-var ENV=production")
+    return parser.parse_args()
 
+def create_spark_session(job_name: str, env_vars: dict) -> pyspark.sql.SparkSession:
+    temp_spark = pyspark.sql.SparkSession.builder.appName(job_name)
     for k, v in env_vars.items():
         os.environ[k] = v
         temp_spark = temp_spark.config(f"spark.appMasterEnv.{k}", v) \
@@ -24,24 +28,15 @@ def create_spark_session(job_name, env_vars):
 
     return temp_spark.getOrCreate()
 
-
-def collect_args(args):
-    dict_args = dict()
-    if args:
-        for arg_str in args:
-            k, v = arg_str.split('=', 1)
-            dict_args[k] = v
+def collect_args(args: list) -> dict:
+    dict_args = {}
+    args = args or []
+    for arg_str in args:
+        key, value = arg_str.split('=', 1)
+        dict_args[key] = value
     return dict_args
 
-
-def main():
-    parser = argparse.ArgumentParser(description='Run a PySpark job')
-    parser.add_argument('--job', type=str, required=True, dest='job_name', help="The name of the job module you want to run.")
-    parser.add_argument('--job-arg', type=str, action='append',
-                        help="Extra arguments to send to the PySpark job, the format is space delimited string. example: --job-arg foo=bar --job-arg date=2012-12-12\ 12:12:12")
-    parser.add_argument('--env-var', type=str, action='append',
-                        help="ENV vars to be passed to cluster. example: --env-var foo=bar --env-var ENV=production")
-    args = parser.parse_args()
+def main(args: argparse.Namespace) -> None:
     job_args = collect_args(args.job_arg)
     env_vars = collect_args(args.env_var)
     print('\nRunning job %s...\njob-args is %s\nenv-vars is %s\n' % (args.job_name, job_args, env_vars))
@@ -58,6 +53,6 @@ def main():
     end = time.time()
     print("\nExecution of job %s took %s seconds" % (args.job_name, end - start))
 
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(args)
--- test/test_entrypoing.py
+++ test/test_entrypoing.py
@@ -1,2 +1,58 @@
-def test_main():
-    pass
+import os
+import sys
+import pyspark
+import pytest
+from entrypoint import collect_args, create_spark_session, parse_arguments
+
+def test_create_spark_session():
+    job_name = "test_job"
+    env_vars = {"ENV": "production"}
+
+    spark_session = create_spark_session(job_name, env_vars)
+    assert isinstance(spark_session, pyspark.sql.SparkSession)
+
+    for k, v in env_vars.items():
+        assert os.environ[k] == v
+        assert spark_session.conf.get(f"spark.appMasterEnv.{k}") == v
+        assert spark_session.conf.get(f"spark.executorEnv.{k}") == v
+
+def test_collect_args():
+    assert collect_args(None) == {}
+    assert collect_args(['foo=bar']) == {'foo': 'bar'}
+    assert collect_args(['foo=bar', 'baz=qux']) == {'foo': 'bar', 'baz': 'qux'}
+    assert collect_args(['foo=bar=baz']) == {'foo': 'bar=baz'}
+
+@pytest.mark.parametrize("args, expected", [
+    (None, {}),
+    (['foo=bar'], {'foo': 'bar'}),
+    (['foo=bar', 'baz=qux'], {'foo': 'bar', 'baz': 'qux'}),
+    (['foo=bar=baz'], {'foo': 'bar=baz'}),
+])
+def test_collect_args_parametrized(args, expected):
+    assert collect_args(args) == expected
+
+def test_parse_arguments():
+    backup = sys.argv
+
+    sys.argv = ['entrypoint.py', '--job', 'test_job']
+    args = parse_arguments()
+    assert args.job_name == 'test_job'
+    assert args.job_arg == None
+    assert args.env_var == None
+
+    sys.argv = ['entrypoint.py', '--job', 'test_job', '--job-arg', 'foo=bar', '--env-var', 'ENV=production']
+    args = parse_arguments()
+    assert args.job_name == 'test_job'
+    assert args.job_arg == ['foo=bar']
+    assert args.env_var == ['ENV=production']
+
+    sys.argv = backup
+
+def test_parse_arguments_failure():
+    backup = sys.argv
+
+    sys.argv = ['entrypoint.py']
+    with pytest.raises(SystemExit):
+        parse_arguments()
+
+    sys.argv = backup
